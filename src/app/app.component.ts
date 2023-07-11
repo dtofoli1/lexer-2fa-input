91a6ef6b275263d1d0babb72d6452bcae0e70052
Dalton Tofoli <daltontofoli1@gmail.com>
2023-07-11 15:41:30 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lexer-2fa-input';
+
+  jumpTo(currentInput:string, nextInput:string, previousInput:string) {
+    var key = eval("event.key;")
+    var tamanho = eval("document.F1."+ currentInput +".value.length;");
+
+                if (tamanho == 1){
+                    if (nextInput != ''){
+                        eval("document.F1."+ nextInput +".focus();");
+                    } else {
+                        alert("POST!!!");
+                        location.reload();
+                    }
+                } else if (key == 'Backspace' && previousInput != ''){
+                    eval("document.F1."+ previousInput +".focus();");
+                } 
+  };
 }
